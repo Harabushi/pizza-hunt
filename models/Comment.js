7497d33c77d9e2cd99af8deffcbdf3d37807a331
Colby Harris <harris.colby@gmail.com>
2022-05-05 03:41:40 -0500

    added reply feature and front-end functionality for single pizza

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,59 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+// We import Types here to let us use a new ID type instead of ObjectId
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    writtenBy: {
+      type: String
+    },
+    replyBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    toJSON: {
+      getters: true
+    }
   }
+);
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
--- models/Pizza.js
+++ models/Pizza.js
@@ -39,7 +39,10 @@ const PizzaSchema = new Schema(
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.length;
+  // the reduce method runs through an array like .map, reduce takes in two inputs, the callback function and the initial value
+  // the callback function has the "current total" on the left, which is either the initial value or the result of the last run
+  // then it has the "current value" on the right, which can be the number in the array, or in this case the comment + replies
+  return this.comments.reduce((total, comment) => total + comment.replies.length +1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
